284d1d6aa732c01d22613a4a521af3d6ae168fca
龚磊 <1029235277@qq.com>
2017-05-17 19:18:24 +0800

    no message

--- Universialshare/我的/controller/我要推店/Controller/GLRecommendStroe_RecordController.m
+++ Universialshare/我的/controller/我要推店/Controller/GLRecommendStroe_RecordController.m
@@ -8,19 +8,24 @@
 
 #import "GLRecommendStroe_RecordController.h"
 #import "GLRecommendStore_RecordCell.h"
+#import "GLRecommendStroe_RecordDateCell.h"
 
-@interface GLRecommendStroe_RecordController ()
+@interface GLRecommendStroe_RecordController ()<UICollectionViewDelegate,UICollectionViewDataSource>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 
 @end
 
 static NSString *ID = @"GLRecommendStore_RecordCell";
+static NSString *ID2 = @"GLRecommendStroe_RecordDateCell";
 @implementation GLRecommendStroe_RecordController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    self.navigationItem.title = @"推店记录";
     [self.tableView registerNib:[UINib nibWithNibName:ID bundle:nil] forCellReuseIdentifier:ID];
+    [self.collectionView registerNib:[UINib nibWithNibName:ID2 bundle:nil] forCellWithReuseIdentifier:ID2];
 }
 
 #pragma UITableViewDelegate UITableViewDataSource
@@ -33,7 +38,9 @@ static NSString *ID = @"GLRecommendStore_RecordCell";
     return cell;
 }
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-    return 100;
+    self.tableView.estimatedRowHeight = 11;
+    self.tableView.rowHeight = UITableViewAutomaticDimension;
+    return self.tableView.rowHeight;
 }
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     self.hidesBottomBarWhenPushed = YES;
@@ -41,4 +48,26 @@ static NSString *ID = @"GLRecommendStore_RecordCell";
 //    [self.navigationController pushViewController:consumerVC animated:YES];
 }
 
+#pragma UICollectionViewDelegate UICollectionViewDataSource
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    
+    return 24;
+}
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+
+    GLRecommendStroe_RecordDateCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:ID2 forIndexPath:indexPath];
+//    cell.backgroundColor = [UIColor redColor];
+
+    return cell;
+    
+}
+//定义每个UICollectionViewCell 的大小
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    return CGSizeMake(100, 50);
+}
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
+    GLRecommendStroe_RecordDateCell *cell = (GLRecommendStroe_RecordDateCell *) [collectionView cellForItemAtIndexPath:indexPath];
+    cell.bgView.backgroundColor = [UIColor greenColor];
+    
+}
 @end
