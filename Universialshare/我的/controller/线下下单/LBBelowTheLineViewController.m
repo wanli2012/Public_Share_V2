ccaf780b7187fb02f75bd387885ecb8e2c982599
龚磊 <1029235277@qq.com>
2017-08-25 14:52:06 +0800

    no message

--- Universialshare/我的/controller/线下下单/LBBelowTheLineViewController.m
+++ Universialshare/我的/controller/线下下单/LBBelowTheLineViewController.m
@@ -210,24 +210,17 @@
 - (NSString *)getRandomStringWithNum:(NSInteger)num
 {
 
-    
     NSString *string = [[NSString alloc]init];
+    
     for (int i = 0; i < num; i++) {
-        int number = arc4random() % 62;
+        int number = arc4random() % 36;
         if (number < 10) {
             
             int figure = arc4random() % 10;
             NSString *tempString = [NSString stringWithFormat:@"%d", figure];
             string = [string stringByAppendingString:tempString];
             
-        }else if(number < 36 && number >10) {
-            
-            int figure = (arc4random() % 26) + 97;
-            char character = figure;
-            NSString *tempString = [NSString stringWithFormat:@"%c", character];
-            string = [string stringByAppendingString:tempString];
-            
-        }else{
+        }else{//大写字母
             
             int figure = (arc4random() % 26) + 65;
             char character = figure;
