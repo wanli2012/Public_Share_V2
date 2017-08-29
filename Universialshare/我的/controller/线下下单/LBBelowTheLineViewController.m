f489ee23d85d1f777c009d949a07a25e34d88748
龚磊 <1029235277@qq.com>
2017-08-29 15:42:26 +0800

    no message

--- Universialshare/我的/controller/线下下单/LBBelowTheLineViewController.m
+++ Universialshare/我的/controller/线下下单/LBBelowTheLineViewController.m
@@ -452,19 +452,18 @@
     UIImagePickerController *picker = [[UIImagePickerController alloc] init];
     //    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
     picker.delegate = self;
-    //    // 设置选择后的图片可以被编辑
-    //    picker.allowsEditing = YES;
-    //    [self presentViewController:picker animated:YES completion:nil];
+    // 设置选择后的图片可以被编辑
     //1.获取媒体支持格式
     NSArray *mediaTypes = [UIImagePickerController availableMediaTypesForSourceType:UIImagePickerControllerSourceTypeSavedPhotosAlbum];
     picker.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
     picker.mediaTypes = @[mediaTypes[0]];
     //5.其他配置
     //allowsEditing是否允许编辑，如果值为no，选择照片之后就不会进入编辑界面
-    picker.allowsEditing = YES;
+    picker.allowsEditing = NO;
     //6.推送
     [self presentViewController:picker animated:YES completion:nil];
 }
+
 -(void)getcamera{
     UIImagePickerControllerSourceType sourceType = UIImagePickerControllerSourceTypeCamera;
     if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
@@ -472,24 +471,25 @@
         UIImagePickerController *picker = [[UIImagePickerController alloc] init];
         picker.delegate = self;
         // 设置拍照后的图片可以被编辑
-        picker.allowsEditing = YES;
+        picker.allowsEditing = NO;
         picker.sourceType = sourceType;
         [self presentViewController:picker animated:YES completion:nil];
     }else {
         
     }
 }
+
 -(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info{
     NSString *type = [info objectForKey:UIImagePickerControllerMediaType];
     if ([type isEqualToString:@"public.image"]) {
         // 先把图片转成NSData
-        UIImage *image = [info objectForKey:@"UIImagePickerControllerEditedImage"];
+        UIImage *image = [info objectForKey:@"UIImagePickerControllerOriginalImage"];
         NSData *data;
         if (UIImagePNGRepresentation(image) == nil) {
             
             data = UIImageJPEGRepresentation(image, 0.1);
         }else {
-            data=    UIImageJPEGRepresentation(image, 0.1);
+            data = UIImageJPEGRepresentation(image, 0.1);
         }
         //#warning 这里来做操作，提交的时候要上传
         // 图片保存的路径
